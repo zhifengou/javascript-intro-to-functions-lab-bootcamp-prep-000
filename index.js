e366725647bcb27965e0953b4c358785b4f0cba1
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 15:59:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string){
 function logWhisper(string){
   return console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
